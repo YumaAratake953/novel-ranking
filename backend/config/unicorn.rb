d9f5527ca0c97ee6bade6dd7a4ae54804f6c026d
YumaAratake953 <asketb03061225@gmail.com>
2022-04-12 17:16:03 +0900

    unicorn.rbのapp_pathを変更

--- backend/config/unicorn.rb
+++ backend/config/unicorn.rb
@@ -1,5 +1,5 @@
 #app_path = File.expand_path('../../', __FILE__)
-app_path = File.expand_path('/var/www/アプリ名')
+app_path = File.expand_path('/var/www/novel-ranking/backend')
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
