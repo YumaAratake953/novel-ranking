3b0ef622f772b5c9cfeb59a5c5de33c866eaa572
YumaAratake953 <asketb03061225@gmail.com>
2022-04-16 18:19:23 +0900

    config / environment/production.rbでconfig.cache_classesをfalseに設定した

--- backend/config/unicorn.rb
+++ backend/config/unicorn.rb
@@ -2,7 +2,7 @@ app_path = File.expand_path('../../', __FILE__)
 # app_path = File.expand_path('/var/www/novel-ranking/backend')
 
 #アプリケーションサーバの性能を決定する
-worker_processes 1
+worker_processes 2
 
 #アプリケーションの設置されているディレクトリを指定
 working_directory app_path
@@ -11,7 +11,8 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+#listen 3000
+listen "#{root_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
