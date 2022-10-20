a4a7c73d5c727272317d470dfa5f5724ad95c779
YumaAratake953 <asketb03061225@gmail.com>
2022-10-20 20:48:58 +0900

    readmeを編集

--- frontend/react-app/src/graphql/client.ts
+++ frontend/react-app/src/graphql/client.ts
@@ -4,8 +4,8 @@ import {
 import { onError } from '@apollo/client/link/error';
 
 const httpLink = createHttpLink({
-   uri: 'https://rails-react-novel.herokuapp.com/graphql'
-  //uri: 'http://localhost:3000/graphql'
+   //uri: 'https://rails-react-novel.herokuapp.com/graphql'
+  uri: 'http://localhost:3000/graphql'
 });
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   const errors = [];
